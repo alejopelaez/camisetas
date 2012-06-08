d6f6b3bf648fd237c1cc27aa961d93859ec28b85
Alejandro Peláez <alejopelaezr@gmail.com>
2012-06-08 18:18:30 -0500

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Camisetas::Application.config.secret_token = '3bfa7ba047038b6181c01d6f46fe42fe5d5dccba2a99ff81112e9e493d3222baf762df5ba5a65f8a82fbb29e60aafadd02add2015d23bc9a3b0892b775a037b6'
